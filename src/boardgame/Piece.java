ea9bdee63493831b7c90c44e741eaa166fcdcb25
Nicolas <nicolasamaralsilva09@gmail.com>
2024-07-03 15:28:38 -0300

    Implementando movimentos possíveis da torre

--- src/boardgame/Piece.java
+++ src/boardgame/Piece.java
@@ -1,17 +1,34 @@
 package boardgame;
 
-public class Piece {
-    protected Position position; //nao seja visivel na camada de xadrez
+public abstract class Piece {
+
+    protected Position position;
     private Board board;
 
     public Piece(Board board) {
         this.board = board;
-        position = null; //enfatizando mas ele ja deixa nulo
+        position = null;
     }
 
-    protected Board getBoard() { //somente classes no mesmo pacote e subclasses vao acessar o tabuleiro de uma peça
+    protected Board getBoard() {
         return board;
     }
 
+    public abstract boolean[][] possibleMoves();
 
+    public boolean possibleMove(Position position) {
+        return possibleMoves()[position.getRow()][position.getColumn()];
+    }
+
+    public boolean isThereAnyPossibleMove() {
+        boolean[][] mat = possibleMoves();
+        for (int i=0; i<mat.length; i++) {
+            for (int j=0; j<mat.length; j++) {
+                if (mat[i][j]) {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
 }
--- src/chess/pieces/Rook.java
+++ src/chess/pieces/Rook.java
@@ -1,6 +1,7 @@
 package chess.pieces;
 
 import boardgame.Board;
+import boardgame.Position;
 import chess.ChessPiece;
 import chess.Color;
 
@@ -13,5 +14,54 @@ public class Rook extends ChessPiece {
     @Override
     public String toString(){
         return "R"; //Letra que vai aparecer no tabuleiro
+    }
+
+    @Override
+    public boolean[][] possibleMoves() {
+        boolean[][] mat = new boolean[getBoard().getRows()][getBoard().getColumns()];
+
+        Position p = new Position(0,0);
+
+        //verificar acima
+        p.setValues(position.getRow() -1, position.getColumn()); //a posição da linha - 1 para analisar a linha de cima
+        while(getBoard().positionExists(p) && !getBoard().thereIsAPiece(p)){ //enquanto a posicao p existir e nao tiver uma peça la
+            mat[p.getRow()][p.getColumn()] = true;
+            p.setRow(p.getRow() - 1);
+        }
+        if (getBoard().positionExists(p) && isThereOpponentPiece(p)){
+            mat[p.getRow()][p.getColumn()] = true;
+        }
+
+        //esquerda
+        p.setValues(position.getRow(), position.getColumn() -1); //a posição da linha - 1 para analisar a linha de cima
+        while(getBoard().positionExists(p) && !getBoard().thereIsAPiece(p)){ //enquanto a posicao p existir e nao tiver uma peça la
+            mat[p.getRow()][p.getColumn()] = true;
+            p.setColumn(p.getColumn() - 1);
+        }
+        if (getBoard().positionExists(p) && isThereOpponentPiece(p)){
+            mat[p.getRow()][p.getColumn()] = true;
+        }
+
+        //direita
+        p.setValues(position.getRow(), position.getColumn() +1); //a posição da linha - 1 para analisar a linha de cima
+        while(getBoard().positionExists(p) && !getBoard().thereIsAPiece(p)){ //enquanto a posicao p existir e nao tiver uma peça la
+            mat[p.getRow()][p.getColumn()] = true;
+            p.setColumn(p.getColumn() + 1);
+        }
+        if (getBoard().positionExists(p) && isThereOpponentPiece(p)){
+            mat[p.getRow()][p.getColumn()] = true;
+        }
+
+        //pra baixo
+        p.setValues(position.getRow() +1, position.getColumn()); //a posição da linha - 1 para analisar a linha de cima
+        while(getBoard().positionExists(p) && !getBoard().thereIsAPiece(p)){ //enquanto a posicao p existir e nao tiver uma peça la
+            mat[p.getRow()][p.getColumn()] = true;
+            p.setRow(p.getRow() + 1);
+        }
+        if (getBoard().positionExists(p) && isThereOpponentPiece(p)){
+            mat[p.getRow()][p.getColumn()] = true;
+        }
+
+        return mat;
     }
 }
